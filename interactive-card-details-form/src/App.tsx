e8ec930c87b556f0d4f95c462d79d148189dc009
CleggOh <andrew.clegg84@gmail.com>
2023-11-10 23:20:21 -0500

    trying another approach

--- interactive-card-details-form/src/App.tsx
+++ interactive-card-details-form/src/App.tsx
@@ -17,8 +17,28 @@ function App() {
   const [cvc, setCvc] = useState("000");
 
   return (
-    <div className="flex items-center">
-      <div className="min-h-screen w-5/12 bg-[url('bg-main-desktop.png')] bg-cover flex items-center">
+    <div className="flex w-[375px] md:w-[1440px] h-screen bg-red-500 bg-[url('bg-main-desktop.png')] bg-no-repeat">
+      <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
+        <div className=" bg-[url('bg-card-front.png')] w-[447px] h-[245px]">
+          <div className="flex gap-x-4 items-center">
+            <div className="rounded-full w-8 h-8 bg-white"></div>
+            <div className="rounded-full w-4 h-4 border-white border-[1px]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    /* <div className=" w-1/2 items-center flex">
+        <div className="w-full flex flex-col items-center">
+          <div className=""></div>
+          <div>
+            <div className="m-2  bg-[url('bg-card-back.png')] w-[447px] h-[245px]" />
+          </div>
+        </div>
+      </div>
+    </div> */
+
+    /* <div className="flex items-center justify-center bg-blue">
+      <div className="min-h-screen w-2/4 bg-[url('bg-main-desktop.png')] bg-cover flex items-center">
         <div className="absolute flex flex-col space-x-2 space-y-3">
           <div className="p-4 left-24 relative h-[150px] w-[280px] text-white rounded-md bg-[url('bg-card-front.png')] bg-cover">
             <div className="flex gap-x-4 items-center">
@@ -46,7 +66,7 @@ function App() {
         </div>
       </div>
 
-      <div className="flex flex-row-reverse space-x-2">
+      <div className="flex flex-row-reverse space-x-2 bg-red-500">
         <form>
           <p>Cardholder Name</p>
           <input
@@ -80,7 +100,7 @@ function App() {
         </form>
         <div className="p-10"></div>
       </div>
-    </div>
+    </div> */
   );
 }
 
